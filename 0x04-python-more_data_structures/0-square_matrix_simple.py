724b52eaea22a0ac01f56ed0be7d9e2179f0411d
kenny7030 <kariukikennedy6@gmail.com>
2023-08-16 08:45:14 -0700

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    
+    for row in matrix:
+        new_row = [x**2 for x in row]
+        new_matrix.append(new_row)
+    
+    return new_matrix
+
+# Example usage
+if __name__ == "__main__":
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    squared_matrix = square_matrix_simple(matrix)
+    
+    for row in squared_matrix:
+        print(row)
+
