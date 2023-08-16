85e7969a008a2366bd79b9c9cf1f9980a16a4db9
kenny7030 <kariukikennedy6@gmail.com>
2023-08-16 10:09:05 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,21 +1,38 @@
+#!/usr/bin/python3
+
+def to_subtract(list_num):
+    to_sub = 0
+    max_list = max(list_num)
+
+    for n in list_num:
+        if max_list > n:
+            to_sub += n
+
+    return max_list - to_sub
+
+
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
+    if not roman_string or not isinstance(roman_string, str):
         return 0
-    
-    roman_values = {
-        'I': 1, 'V': 5, 'X': 10, 'L': 50,
-        'C': 100, 'D': 500, 'M': 1000
-    }
-    
-    total = 0
-    prev_value = 0
-    
-    for char in roman_string[::-1]:
-        value = roman_values[char]
-        if value >= prev_value:
-            total += value
-        else:
-            total -= value
-        prev_value = value
-    
-    return total
+
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    num = 0
+    last_rom = 0
+    list_num = [0]
+
+    for ch in roman_string:
+        for r_num in rom_n:
+            if r_num == ch:
+                if rom_n[ch] <= last_rom:
+                    num += to_subtract(list_num)
+                    list_num = [rom_n[ch]]
+                else:
+                    list_num.append(rom_n[ch])
+
+                last_rom = rom_n[ch]
+
+    num += to_subtract(list_num)
+
+    return num
+
