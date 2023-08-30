a0179ce9f087177c137301f5d3b0ea2fec2ea87a
kenny7030 <kariukikennedy6@gmail.com>
2023-08-30 15:10:54 -0700

    0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    This is the Square class.
+    It defines a square.
+    """
+
+    def __init__(self):
+        """
+        This is the constructor method.
+        It initializes the Square object.
+        """
+        pass
