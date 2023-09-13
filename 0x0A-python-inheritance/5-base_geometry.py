3b9890a375940d666ee246c8115a1d74aa10b1f5
kenny7030 <kariukikennedy6@gmail.com>
2023-09-13 15:02:37 -0700

    5-base_geometry.py

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module containing the ``BaseGeometry`` class definition.
+"""
+
+
+class BaseGeometry:
+    """``BaseGeometry`` class definition.
+    """
+
+    def area(self):
+        """Calculate the area of a shape."""
+        pass
